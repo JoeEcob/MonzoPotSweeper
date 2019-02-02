d9bcc12afcc2812453d5099668fd40b48188f348
Joe Ecob <me@joeecob.uk>
2019-02-02 18:50:49 +0000

    Add project files.

---
+++ MonzoPotSweeper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonzoPotSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
